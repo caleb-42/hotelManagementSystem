fd97412820a463755e7e97d039eebd345e131de8
tegovona <tegovona@gmail.com>
2018-10-08 14:18:32 +0100

    restaurant_logoff.php fixed, restaurant_discount fixed lower_limit issue

--- assets/php1/restaurant_bar/restaurant_logoff.php
+++ assets/php1/restaurant_bar/restaurant_logoff.php
@@ -7,17 +7,21 @@ $role = $_SESSION['role'];
 
 $sql_getID = "SELECT MAX(id) AS id FROM restaurant_sessions WHERE user_name='$user_name' AND role = '$role'";
 $getID_result = mysqli_query($dbConn, $sql_getID);
+var_dump($getID_result);
 $max_ID = mysqli_fetch_array($getID_result);
 $ID = $max_ID["id"];
 $ID = intval($ID);
+var_dump($ID);
 
-$sql_logout = "UPDATE restaurant_sessions SET logged_off_time = CURRENT_TIMESTAMP , logged_on_state = 'LOGGED OFF' WHERE username='$user_name' AND id = $ID";
-mysqli_query($dbConn, $sql_logout);
+$sql_logout = "UPDATE restaurant_sessions SET logged_off_time = CURRENT_TIMESTAMP , logged_on_state = 'LOGGED OFF' WHERE user_name='$user_name' AND id = $ID";
+$sql_logout_result = mysqli_query($dbConn, $sql_logout);
 unset($_SESSION['user_name']);
 unset($_SESSION['role']);
 session_unset();
 session_destroy();
  
-header("Location:  ../../../logIn.php"); 
+//header("Location:  ../../../logIn.php"); 
+var_dump($sql_logout_result);
+echo mysqli_error($dbConn);
 exit();
 ?>
